ef0abd60732ca6f8dbdcab6454c9fd8cc5f35e85
nguyen vinh linh <nguyenvinhlinh@localhost.localdomain>
2017-04-01 01:11:54 +0700

    READDY TO DEPLOY

--- web/static/js/images/show.js
+++ web/static/js/images/show.js
@@ -1,8 +1,15 @@
 var Image = {
   initial_image_show_page: function(){
-    console.log("init success");
+    var clipboard = new Clipboard("button[for='copy-clipboard']");
+    clipboard.on('success', function(e) {
+      $(".image-container").notify("Copied", {
+        className: "success",
+        position: 'bottom right'
+      });
+    });
   }
 }
+
 module.exports = {
   Image: Image
 }
--- web/views/image_view.ex
+++ web/views/image_view.ex
@@ -1,11 +1,11 @@
 defmodule ImageSharing.ImageView do
   use ImageSharing.Web, :view
 
-  def get_relative_url(image) do
+  def get_image_relative_url(image) do
     "/uploads/#{image.filename}"
   end
 
-  def get_absolute_url(image) do
+  def get_image_absolute_url(image) do
     Application.get_env(:image_sharing, :app_root_url) <> "/uploads/#{image.filename}"
   end
 end
